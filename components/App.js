7ba8cfaa25d743229bb5783820dfb43712a9dad5
limziyun <39241304+limziyun@users.noreply.github.com>
2018-10-23 15:46:14 +0800

    Function added

--- components/App.js
+++ components/App.js
@@ -23,7 +23,8 @@ export default {
     };
   },
   methods: {
-    updateCurrentChart: function(newChart) {}
+    updateCurrentChart: function(newChart) {},
+    something: function(){}
   },
   firebase: {
     charts: {
